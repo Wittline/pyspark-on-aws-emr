64d7915db42740cdb9c9dac0c494dd4a5d529451
Wittline <8701464+Wittline@users.noreply.github.com>
2021-06-09 20:07:49 -0500

    Create bootstrap-action.sh

---
+++ code/bootstrap-action.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x -e
+
+echo -e 'export PYSPARK_PYTHON=/usr/bin/python3 
+export HADOOP_CONF_DIR=/etc/hadoop/conf 
+export SPARK_JARS_DIR=/usr/lib/spark/jars 
+export SPARK_HOME=/usr/lib/spark' >> $HOME/.bashrc && source $HOME/.bashrc
+
+sudo python3 -m pip install awscli boto spark-nlp
+
+set +x
+exit 0
