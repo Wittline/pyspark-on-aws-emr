08601e0409cdfb4f14059e58d15e51661b6e090e
Wittline <8701464+Wittline@users.noreply.github.com>
2021-06-14 17:07:58 -0500

    Updating

--- code/bootstrap-action.sh
+++ code/bootstrap-action.sh
@@ -6,7 +6,7 @@ export HADOOP_CONF_DIR=/etc/hadoop/conf
 export SPARK_JARS_DIR=/usr/lib/spark/jars 
 export SPARK_HOME=/usr/lib/spark' >> $HOME/.bashrc && source $HOME/.bashrc
 
-sudo python3 -m pip install spark-nlp
+sudo python3 -m pip install awscli boto spark-nlp
 
 set +x
 exit 0
